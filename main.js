64508bfa5b31e0e14217d276cdfcde7fb5d2fad7
ilushacomeback <ilya.rudenko.2105@mail.ru>
2024-03-12 23:26:58 +0300

    test

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
-import CurrencyAPI from "./node_modules/@everapi/currencyapi-js/index.js"
-const currencyApi = new CurrencyAPI("cur_live_mjssoAewc2eBDAW1qAQK27TDYh7ILgh78rEwKBCE");
+class CurrencyAPI {
+    baseUrl = 'https://api.currencyapi.com/v3/';
+
+    constructor(apiKey = '') {
+        this.headers = {
+            apikey: apiKey
+        };
+    }
+
+    call (endpoint, params = {}) {
+        const paramString = new URLSearchParams({
+            ...params
+        }).toString();
+
+        return fetch(`${this.baseUrl}${endpoint}?${paramString}`, { headers: this.headers })
+            .then(response => response.json())
+            .then(data => {
+                return data;
+            });
+    }
+
+    status () {
+        return this.call('status');
+    }
+
+    currencies (params) {
+        return this.call('currencies', params);
+    }
+
+    latest (params) {
+        return this.call('latest', params);
+    }
+
+    historical (params) {
+        return this.call('historical', params);
+    }
+
+    range (params) {
+        return this.call('range', params);
+    }
+
+    convert (params) {
+        return this.call('convert', params);
+    }
+}
+
+const currencyApi = new CurrencyAPI('cur_live_mjssoAewc2eBDAW1qAQK27TDYh7ILgh78rEwKBCE')
 
 const curValute = document.querySelector('select[name=currencyOne]')
 const nextValute = document.querySelector('select[name=currencyTwo]')
