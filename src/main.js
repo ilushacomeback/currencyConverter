b8d88149e3daac8a562f5ff59647553d905320bf
ilushacomeback <ilya.rudenko.2105@mail.ru>
2024-03-12 19:15:15 +0300

    add name error

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
-import CurrencyAPI from '@everapi/currencyapi-js'
+import CurrencyAPI from "@everapi/currencyapi-js"; /*Не удалось найти файл объявления модуля "@everapi/currencyapi-js". 
+"c:/Users/USER/Desktop/pet_proj/currency_converter/node_modules/@everapi/currencyapi-js/index.js" имеет неявный тип "any".
+Попробуйте использовать команду "npm i --save-dev @types/everapi__currencyapi-js", если он существует,
+или добавьте новый файл объявления (.d.ts), содержащий "declare module '@everapi/currencyapi-js';".ts(7016) */
 
-const currencyApi = new CurrencyAPI('cur_live_mjssoAewc2eBDAW1qAQK27TDYh7ILgh78rEwKBCE')
+const currencyApi = new CurrencyAPI(
+  "cur_live_mjssoAewc2eBDAW1qAQK27TDYh7ILgh78rEwKBCE"
+);
